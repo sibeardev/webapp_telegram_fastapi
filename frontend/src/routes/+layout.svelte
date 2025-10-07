fa521ca7f89c0b2ae09ec3462c41e380edcc2e8f
ILYA NIKOLAENKO <dad.siberian@gmail.com>
2025-10-07 16:31:25 +0300

    feat: Added telegram styles

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg";
   import "../app.css";
+  import { onMount } from "svelte";
+  import WebApp from "@twa-dev/sdk";
+
   let { children } = $props();
+  onMount(() => {
+    WebApp.ready();
+  });
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<main class="min-h-screen flex items-center justify-center p-4">
+<main class="min-h-screen flex items-center justify-center p-4 tg-bg tg-text">
   {@render children?.()}
 </main>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -28,8 +28,8 @@
   });
 </script>
 
-<div class="bg-gray-800 p-6 rounded-2xl shadow-lg max-w-md w-full">
-  <h1 class="text-2xl font-bold mb-4 text-center">Telegram WebApp</h1>
+<div class="tg-secondary-bg p-6 rounded-2xl shadow-lg max-w-md w-full">
+  <h1 class="tg-accent-text text-2xl font-bold mb-4 text-center">Profile</h1>
   {#if errorMessage}
     <p class="text-red-500 text-center">{errorMessage}</p>
   {:else if user}
@@ -37,13 +37,13 @@
       <div class="flex justify-center mb-4">
         {#if user.photo_url}
           <img
-            class="w-20 h-20 rounded-full object-cover border-2 border-gray-700 shadow-lg"
+            class="w-24 h-24 rounded-full object-cover border-2 shadow-lg"
             src={user.photo_url}
             alt={user.username}
           />
         {:else}
           <div
-            class="relative w-20 h-20 flex items-center justify-center overflow-hidden bg-gray-700 rounded-full shadow-lg"
+            class="relative w-24 h-24 flex items-center justify-center overflow-hidden bg-gray-700 rounded-full shadow-lg"
           >
             <svg
               class="w-12 h-12 text-gray-400"
@@ -60,20 +60,32 @@
           </div>
         {/if}
       </div>
-      <p><span class="font-semibold">ID:</span> {user.user_id}</p>
-      <p><span class="font-semibold">First name:</span> {user.first_name}</p>
+      <p><span class="font-semibold tg-hint-text">ID:</span> {user.user_id}</p>
+      <p>
+        <span class="font-semibold tg-hint-text">First name:</span>
+        {user.first_name}
+      </p>
       {#if user.last_name}
-        <p><span class="font-semibold">Last name:</span> {user.last_name}</p>
+        <p>
+          <span class="font-semibold tg-hint-text">Last name:</span>
+          {user.last_name}
+        </p>
       {/if}
       {#if user.username}
-        <p><span class="font-semibold">Username:</span> @{user.username}</p>
+        <p>
+          <span class="font-semibold tg-hint-text">Username:</span>
+          @{user.username}
+        </p>
       {/if}
-      <p><span class="font-semibold">Language:</span> {user.language_code}</p>
+      <p>
+        <span class="font-semibold tg-hint-text">Language:</span>
+        {user.language_code}
+      </p>
       {#if user.is_premium}
         <p class="text-yellow-400 font-semibold">⭐ Premium user</p>
       {/if}
     </div>
   {:else}
-    <p class="text-gray-400 text-center">User not found</p>
+    <p class="tg-destructive-text text-center">User not found</p>
   {/if}
 </div>
