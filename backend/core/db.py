3080c54bd31aefcd626c7c4d72317c4648ccb5b9
ILYA NIKOLAENKO <dad.siberian@gmail.com>
2025-10-28 11:22:04 +0300

    fix: update database connection to use PROJECT_NAME

--- backend/core/db.py
+++ backend/core/db.py
@@ -2,10 +2,10 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from bot.models import User
-from core.config import MONGO_DSN
+from core.config import MONGO_DSN, PROJECT_NAME
 
 
 async def init_db():
     client = AsyncIOMotorClient(MONGO_DSN)
-    db = client.ai_tracker
+    db = client[PROJECT_NAME]
     await init_beanie(database=db, document_models=[User])  # type: ignore
